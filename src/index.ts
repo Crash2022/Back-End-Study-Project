ca1d3a62a1294f6bc24f7549b949366ebe3b06af
Crash2022 <crash555@mail.ru>
2023-02-06 12:37:12 +0500

    add: get params and query

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,69 @@ import express, {Request, Response} from 'express'
 const app = express()
 const port = 3000
 
-const products = [{title: 'mango'}, {title: 'orange'}]
-const addresses = [{value: 'Sovetskaya, 17'}, {title: 'Naberejnaya, 10'}]
+const products = [{id: '1', title: 'mango'}, {id: '2', title: 'orange'}]
+const addresses = [{id: '1', value: 'Sovetskaya, 17'}, {id: '2', title: 'Naberejnaya, 10'}]
 
 // get requests
+// get PRODUCTS
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello Samurai!')
 })
 
+// query params
 app.get('/products', (req: Request, res: Response) => {
-    res.send(products)
+    if (req.query.title) {
+        let searchString = req.query.title.toString()
+        // res.send(products.filter(p => p.title.indexOf(searchString) > -1))
+        res.send(products.filter(p => p.title.includes(searchString)))
+    } else {
+        res.send(products)
+    }
+})
+// params
+app.get('/products/:id', (req: Request, res: Response) => {
+    let product = products.find(el => el.id === req.params.id)
+    // если id будет цифрой, то надо перевести в строку
+    // let product = products.find(el => el.title === +req.params.id)
+
+    if(product) {
+        res.send(product)
+    } else {
+        res.send(404)
+    }
 })
 app.get('/products/:productTitle', (req: Request, res: Response) => {
-    res.send(products.find(el => el.title === req.params.productTitle))
+    let product = products.find(el => el.title === req.params.productTitle)
+    // если id будет цифрой, то надо перевести в строку
+    // let product = products.find(el => el.title === +req.params.productTitle)
+
+    if(product) {
+        res.send(product)
+    } else {
+        res.send(404)
+    }
 })
 
+// delete PRODUCTS
+
+
+
+//---------------------------------------------------------------------
+
+// get ADDRESSES
 app.get('/addresses', (req: Request, res: Response) => {
     res.send(addresses)
+})
+app.get('/addresses/:id', (req: Request, res: Response) => {
+    let address = addresses.find(el => el.id === req.params.id)
+    // если id будет цифрой, то надо перевести в строку
+    // let address = addresses.find(el => el.id === +req.params.id)
+
+    if(address) {
+        res.send(address)
+    } else {
+        res.send(404)
+    }
 })
 
 // start app
