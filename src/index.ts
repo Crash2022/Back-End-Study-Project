6120b00af5f8d8787a6eaca18e5190fdc5881992
Crash2022 <crash555@mail.ru>
2023-02-06 21:16:01 +0500

    add: middleware to app

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from 'express'
+import express, {NextFunction, Request, Response} from 'express'
 import bodyParser from 'body-parser'
 import {productsRouter} from './routes/products-router'
 import {addressesRouter} from './routes/addresses-router'
@@ -11,8 +11,38 @@ const port = 3000
 // const port = process.env.PORT || 3000
 
 // при отправке post запроса необходимо распарсить body-post
+// подключение Middleware ко всему приложению
 const parserMiddleware = bodyParser({})
 app.use(parserMiddleware)
+
+//----------------------------------------------------
+
+let authGuardMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    // @ts-ignore
+    if (req.query.token === '123') {
+        next()
+    } else {
+        res.send(401)
+    }
+
+}
+// app.use(authGuardMiddleware)
+
+let counterRequest = 0
+let counterMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    counterRequest++
+    next()
+}
+// app.use(counterMiddleware)
+
+// пример подключения Middleware к конкретному роуту
+app.get('/products', counterMiddleware, (req: Request, res: Response) => {
+    // @ts-ignore
+    const title = req.title;
+    res.send({value: 'Counter: ' + counterRequest});
+})
+
+//----------------------------------------------------
 
 // стартовая страница
 app.get('/', (req: Request, res: Response) => {
