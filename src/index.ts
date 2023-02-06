bb5d9e1e46844348be7b2d68a57fba842d0fca64
Crash2022 <crash555@mail.ru>
2023-02-06 11:58:29 +0500

    add: new get requests

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
 // https://back-end-study-project.vercel.app/
 
+// create express app
 import express, {Request, Response} from 'express'
 const app = express()
 const port = 3000
 
+const products = [{title: 'mango'}, {title: 'orange'}]
+const addresses = [{value: 'Sovetskaya, 17'}, {title: 'Naberejnaya, 10'}]
+
+// get requests
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World! Deploy to Vercel!')
+    res.send('Hello Samurai!')
 })
 
+app.get('/products', (req: Request, res: Response) => {
+    res.send(products)
+})
+app.get('/products/:productTitle', (req: Request, res: Response) => {
+    res.send(products.find(el => el.title === req.params.productTitle))
+})
+
+app.get('/addresses', (req: Request, res: Response) => {
+    res.send(addresses)
+})
+
+// start app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
